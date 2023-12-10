bbb90c8ea03b64a81151d4e5ea7024ea2807d013
Hamersly <maksimwanderer96@gmail.com>
2023-12-10 22:41:05 +0500

    debug env

--- helpers/getContent.ts
+++ helpers/getContent.ts
@@ -1,23 +1,26 @@
+export const NEXT_URL =
+  process.env.NEXT_PUBLIC_FRONTEND_URL || process.env.NEXT_PUBLIC_URL;
+
 export const getTopList = async (): Promise<object> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/top`);
+  const response = await fetch(`${NEXT_URL}/api/top`);
   if (!response.ok) throw new Error('Unable to fetch topList.');
   return response.json();
 };
 
 export const getContentList = async (format: string, sort: string, listPage: number = 1): Promise<object> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/list?format=${format}&sort=${sort}&listPage=${listPage}`);
+  const response = await fetch(`${NEXT_URL}/api/list?format=${format}&sort=${sort}&listPage=${listPage}`);
   if (!response.ok) throw new Error('Unable to fetch topList.');
   return response.json();
 };
 
 export const getDetail = async (format: string | null, id: string | null): Promise<object> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/detail?format=${format}&id=${id}`);
+  const response = await fetch(`${NEXT_URL}/api/detail?format=${format}&id=${id}`);
   if (!response.ok) throw new Error('Unable to fetch detail content.');
   return response.json();
 };
 
 export const getSearchResult = async (query: string | null, queryPage: number = 1): Promise<object> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/search?queryPage=${queryPage}&query=${query}`);
+  const response = await fetch(`${NEXT_URL}/api/search?queryPage=${queryPage}&query=${query}`);
   if (!response.ok) throw new Error('Unable to fetch detail content.');
   return response.json();
 };
